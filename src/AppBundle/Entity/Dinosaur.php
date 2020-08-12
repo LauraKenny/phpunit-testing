6aad16f0a2aa29e2025f0e3787bdc9a036e4e89e
Laura Kenny <laura.kenny@oetkerdigital.com>
2020-08-12 13:57:46 +0200

    First commit

---
+++ src/AppBundle/Entity/Dinosaur.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="dinosaurs")
+ */
+class Dinosaur
+{
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $length = 0;
+
+    /**
+     * @ORM\Column(type="string")
+     */private $genus;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */private $isCarnivorous;
+
+    public function __construct(string $genus = 'Unknown', bool $isCarnivorous = true)
+    {
+        $this->genus = $genus;
+        $this->isCarnivorous = $isCarnivorous;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLength(): int
+    {
+        return $this->length;
+    }
+
+    /**
+     * @param int $length
+     */
+    public function setLength(int $length)
+    {
+        $this->length = $length;
+    }
+
+    public function getSpecification(): string
+    {
+        return sprintf(
+            'This %s %scarnivorous dinosaur is %d meters long',
+            $this->genus,
+            $this->isCarnivorous ? '' : 'non-',
+            $this->length
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getGenus(): string
+    {
+        return $this->genus;
+    }
+
+}
