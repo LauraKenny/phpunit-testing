3c4015a134e34753269c15809b238c5cb6530a55
Laura Kenny <laura.kenny@oetkerdigital.com>
2020-09-08 13:51:51 +0200

    Added partial mocking example

--- tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
+++ tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
@@ -4,30 +4,49 @@
 namespace Tests\AppBundle\Service;
 
 use AppBundle\Entity\Dinosaur;
+use AppBundle\Entity\Enclosure;
+use AppBundle\Entity\Security;
+use AppBundle\Factory\DinosaurFactory;
 use AppBundle\Service\EnclosureBuilderService;
 use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class EnclosureBuilderServiceIntegrationTest extends KernelTestCase
 {
-    public function testItBuildsEnclosureWithDefaultSpecification()
+    protected function setUp()
     {
         self::bootKernel();
 
+        $this->truncateEntities([
+           Enclosure::class,
+           Security::class,
+           Dinosaur::class
+        ]);
+    }
+
+    public function testItBuildsEnclosureWithDefaultSpecification()
+    {
         /**
          * @var EnclosureBuilderService $enclosureBuilderService
          */
-        $enclosureBuilderService = self::$kernel->getContainer()
-            ->get('test.'.EnclosureBuilderService::class);
+//        $enclosureBuilderService = self::$kernel->getContainer()
+//            ->get('test.'.EnclosureBuilderService::class);
+
+        $dinoFactory = $this->createMock(DinosaurFactory::class);
+        $dinoFactory->expects($this->any())
+            ->method('growFromSpecification')
+            ->willReturnCallback(function($spec) {
+                return new Dinosaur();
+            });
+
+        $enclosureBuilderService = new EnclosureBuilderService(
+            $this->getEntityManager(),
+            $dinoFactory
+        );
 
         $enclosureBuilderService->buildEnclosure();
 
-        /**
-         * @var EntityManager $em
-         */
-        $em = self::$kernel->getContainer()
-            ->get('doctrine')
-            ->getManager();
+        $em = $this->getEntityManager();
 
         $count = (int) $em->getRepository(Security::class)
             ->createQueryBuilder('s')
@@ -44,5 +63,37 @@ class EnclosureBuilderServiceIntegrationTest extends KernelTestCase
             ->getSingleScalarResult();
 
         $this->assertSame(3, $count, 'Amount of dinosaurs is not the same');
+    }
+
+    private function truncateEntities(array $entities)
+    {
+        $connection = $this->getEntityManager()->getConnection();
+        $databasePlatform = $connection->getDatabasePlatform();
+
+        if ($databasePlatform->supportsForeignKeyConstraints()) {
+            $connection->query('SET FOREIGN_KEY_CHECKS=0');
+        }
+
+        foreach ($entities as $entity) {
+            $query = $databasePlatform->getTruncateTableSQL(
+                $this->getEntityManager()->getClassMetadata($entity)->getTableName()
+            );
+
+            $connection->executeUpdate($query);
+        }
+
+        if ($databasePlatform->supportsForeignKeyConstraints()) {
+            $connection->query('SET FOREIGN_KEY_CHECKS=1');
+        }
+    }
+
+    /**
+     * @return EntityManager
+     */
+    private function getEntityManager()
+    {
+        return self::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
     }
 }
